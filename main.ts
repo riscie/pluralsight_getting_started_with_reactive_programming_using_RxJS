7a9d213f05586b2a4f7c2de43b9b7851c5304923
Matthias Langhard <matthias.langhard@ecologic.ch>
2018-03-31 15:23:05 +0200

    Creating Your First Observable and Observer

--- main.ts
+++ main.ts
@@ -1,1 +1,20 @@
-alert('ok!')
+import { Observable, Observer } from "rxjs";
+
+let numbers = [1, 2, 40, 66, 98];
+let source = Observable.from(numbers);
+
+class MyObserver implements Observer<number> {
+    next(value) {
+        console.log(`value: ${value}`);
+    }
+
+    error(err) {
+        console.log(`error: ${err}`);
+    }
+
+    complete() {
+        console.log('complete');
+    }
+}
+
+source.subscribe(new MyObserver());
