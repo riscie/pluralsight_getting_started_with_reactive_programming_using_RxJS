8d08fc4a12130eb9eae5f0b4c1a07e2642f81db6
Matthias Langhard <matthias.langhard@ecologic.ch>
2018-03-31 15:34:58 +0200

    Using Observable.create

--- main.ts
+++ main.ts
@@ -1,7 +1,13 @@
 import { Observable } from "rxjs";
 
 let numbers = [1, 2, 40, 66, 98];
-let source = Observable.from(numbers);
+let source = Observable.create(observer => {
+    for (let n of numbers) {
+        observer.next(n);
+    }
+    observer.complete();
+});
+
 
 source.subscribe(
     value => console.log(`value: ${value}`),
