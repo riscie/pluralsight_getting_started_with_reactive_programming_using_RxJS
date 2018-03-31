9c3bb397eaed671a44eb8546d64952638cc5331c
Matthias Langhard <matthias.langhard@ecologic.ch>
2018-03-31 15:28:38 +0200

    An Easier Observer

--- main.ts
+++ main.ts
@@ -1,20 +1,10 @@
-import { Observable, Observer } from "rxjs";
+import { Observable } from "rxjs";
 
 let numbers = [1, 2, 40, 66, 98];
 let source = Observable.from(numbers);
 
-class MyObserver implements Observer<number> {
-    next(value) {
-        console.log(`value: ${value}`);
-    }
-
-    error(err) {
-        console.log(`error: ${err}`);
-    }
-
-    complete() {
-        console.log('complete');
-    }
-}
-
-source.subscribe(new MyObserver());
+source.subscribe(
+    value => console.log(`value: ${value}`),
+    err => console.log(`error: ${err}`),
+    () => console.log('complete')
+);
