a9000b62358ff3411ef0638aa57716ebb24d0753
Matthias Langhard <matthias.langhard@ecologic.ch>
2018-04-04 10:23:52 +0200

    retry operator example

--- main.ts
+++ main.ts
@@ -1,16 +1,23 @@
 import { Observable } from "rxjs";
 
-let visitors = ["Namita", "Amit", "Rohit", "Neetika"];
-let source = Observable.from(visitors)
-    .map(v => Observable.of('Hello ' + v));
+let source = Observable.create(observer => {
+    [1, 2, 3, 4]
+        .map(n => n * (Math.random() * 2))
+        .forEach((n, i) => {
+            observer.next(`${i + 1}: ${n}`);
+            if (n > 4) {
+                const msg = "value > 4 not allowed!";
+                console.error(msg);
+                observer.error(msg);
+            }
+        });
+    observer.complete();
+});
 
-source.mergeAll()
-    .subscribe(v => console.log(v));
+source
+    .retry(5)
+    .subscribe(v => console.log(v),
+        err => console.error(err),
+        () => console.log('complete'));
 
-/* Output:
-    Hello Namita
-    Hello Amit
-    Hello Rohit
-    Hello Neetika
- */
 
